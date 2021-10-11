d245fd320538dcad0c8ea3dda208cd97bd428f14
Matt Armstrong <matt@rfc20.org>
2021-10-11 16:51:31 -0700

    Name routes more conveniently

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+
+  # The following could be expressed more simply as:
+  #
+  #   get 'static_pages/help'
+  #   get 'static_pages/about'
+  #   get 'static_pages/contact'
+  #
+  # The form below merely creates routes with shorter names, such as
+  # 'home_path' and 'home_uri', which can lead to more concise use in ERB
+  # templates.
+
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end

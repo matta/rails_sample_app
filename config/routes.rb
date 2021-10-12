a38964a0f2fbdfa326a13af0c2eb5eca46012570
Matt Armstrong <matt@rfc20.org>
2021-10-12 14:20:09 -0700

    Add new user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  get '/signup', to: 'users#new'
 
   # The following could be expressed more simply as:
   #
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  def verify_navbar_links(path, template)
-    get path
+  def verify_navbar_links(template)
     assert_template template
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
@@ -11,18 +10,28 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   end
 
   test "root navbar links" do
-    verify_navbar_links(root_path, 'static_pages/home')
+    get root_path
+    verify_navbar_links('static_pages/home')
+    assert_select "a[href=?]", signup_path
   end
 
   test "about navbar links" do
-    verify_navbar_links(about_path, 'static_pages/about')
+    get about_path
+    verify_navbar_links('static_pages/about')
   end
 
   test "contact navbar links" do
-    verify_navbar_links(contact_path, 'static_pages/contact')
+    get contact_path
+    verify_navbar_links('static_pages/contact')
   end
 
   test "help navbar links" do
-    verify_navbar_links(help_path, 'static_pages/help')
+    get help_path
+    verify_navbar_links('static_pages/help')
+  end
+
+  test "signup navbar links" do
+    get signup_path
+    verify_navbar_links('users/new')
   end
 end

7420153f268810ee9cc802c65c100b15b756e291
Matt Armstrong <matt@rfc20.org>
2021-10-12 19:31:12 -0700

    Insist that User email is unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,14 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
+            # There is also a considerationn with respect to case
+            # sensitivity in the DB:
+            # https://stackoverflow.com/questions/7948501/case-insensitive-unique-index-in-rails-activerecord/10660412
+            uniqueness: true
+
+  before_validation :downcase_email
+  def downcase_email
+    self.email = email.downcase if email
+  end
+
 end

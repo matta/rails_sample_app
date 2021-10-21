866b64b7f92b3d3f5c1e133fd0910e775080c3f8
Matt Armstrong <matt@rfc20.org>
2021-10-21 11:00:22 -0700

    Handle successful updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      # Handle successful update
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end

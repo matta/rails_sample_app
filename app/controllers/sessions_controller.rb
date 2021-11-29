3f91e55c6eaa2aa40c750315c242fe85958732e2
Matt Armstrong <matt@rfc20.org>
2021-11-29 12:14:25 -0800

    more account activation stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,25 @@ class SessionsController < ApplicationController
     email = params[:session][:email].downcase
     password = params[:session][:password]
     user = User.find_by(email: email)
-    if user&.authenticate(password)
-      # Log the user in and redirect to the forwarding URL or the user's
-      # show page.
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      if params[:session][:remember_me] == '1'
-        remember user
+    if user && user.authenticate(password)
+      if user.activated?
+        # Log the user in and redirect to the forwarding URL or the user's
+        # show page.
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        if params[:session][:remember_me] == '1'
+          remember user
+        else
+          forget user
+        end
+        log_in user
+        redirect_to forwarding_url || user
       else
-        forget user
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
       end
-      log_in user
-      redirect_to forwarding_url || user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
